43feae14a581ae65cbfc8fe08d02b7a7c53ead93
Nitin Gupta <nitin.gupta@shephertz.com>
2014-03-05 15:03:34 +0530

    Added

---
+++ AppWarpChatDemo/AppWarpChatDemo/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  AppWarpChatDemo
+//
+//  Created by Nitin Gupta on 04/03/14.
+//  Copyright (c) 2014 Nitin Gupta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppWarp_iOS_SDK/AppWarp_iOS_SDK.h>
+
+static NSString *apiKey = @"1adc4a4cf9563445d1326da3485f74ec7e9286542f1f3c6b36691d100fb3c592";
+static NSString *secretKey =  @"f11b0f2a47a699c7e5c5677a4807c53dbb98a60a08ba9598b74adf64a3616557";
+
+#define kFunctionLog     NSLog(@"%s",__FUNCTION__)
---
+++ AppWarpChatDemo/AppWarpChatDemo/SampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SampleAppDelegate.h
+//  AppWarpChatDemo
+//
+//  Created by Nitin Gupta on 04/03/14.
+//  Copyright (c) 2014 Nitin Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
